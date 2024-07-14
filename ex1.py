dbdde3684f6e9e7d998d92728f899bb10a3366df
Shivangi-66 <shivangibhosle12@gmail.com>
2024-07-14 16:38:13 +0530

    example

--- ex1.py
+++ ex1.py
@@ -1,1 +1,4 @@
-print("abc")
+print ("Name Marks Age")
+print ("%s %14.7f %11d" %("Shivangi Bhosle" , 80.67 , 27))
+print("%s %12.2f %11d" %("Khushi sahita", 81.00,28))
+print("%s %13.3f %11d" %("Tejas Patil" , 82.00,27))
